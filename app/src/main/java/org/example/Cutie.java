8c5dc7c44bd5746748953c8805d25bb0ec868002
nunezag816 <nunezag@msoe.edu>
2025-07-20 21:41:09 -0500

    Update Cutie.java

--- app/src/main/java/org/example/Cutie.java
+++ app/src/main/java/org/example/Cutie.java
@@ -1,7 +1,31 @@
 package org.example;
 
-public interface Cutie {
-  public String description(); // All cuties need to have a description of what makes them cute.
+public class Puppy implements Cutie {
+    public String description() {
+        return "A little puppy with big, sad eyes";
+    }
 
-  public Integer cutenessRating(); // All cuties get a cuteness rating out of ten.
+    public Integer cutenessRating() {
+        return 11;
+    }
+}
+
+public class Kitty implements Cutie {
+    public String description() {
+        return "A kitten with tiny paws and whiskers";
+    }
+
+    public Integer cutenessRating() {
+        return 10;
+    }
+}
+
+public class PygmyMarmoset implements Cutie {
+    public String description() {
+        return "A teeny-tiny monkey with big eyes";
+    }
+
+    public Integer cutenessRating() {
+        return 9;
+    }
 }
